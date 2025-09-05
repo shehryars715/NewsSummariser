6904e4ac1a1b22397011d9525cb60e7fcd583462
Shehryar Ali <shehryar0707@gmail.com>
2025-09-06 01:01:00 +0500

    cleaned

--- backend/test.py
+++
@@ -1,6 +0,0 @@
-from rag_api import get_article_by_url
-
-
-
-r= get_article_by_url("https://www.dawn.com/news/1939445/pakistan-dispatches-105-tonnes-of-humanitarian-aid-for-quake-struck-afghanistan")
-print(r)
